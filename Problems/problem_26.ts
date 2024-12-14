a7366c3c101c5bbc520470c639c77d2c80f8bb93
unknown <s88475@bht-berlin.de>
2024-12-14 17:49:20 +0100

    Had Problems but manage to do it

--- Problems/problem_26.ts
+++ Problems/problem_26.ts
@@ -1,3 +1,18 @@
 function removeDuplicates(nums: number[]): number {
-    return 0
-};
+    
+    // Empty Array
+    if (nums.length === 0) {
+        return 0; 
+    }
+
+    let index = 1;  
+    for (let i = 1; i < nums.length; i++) {
+        // If the current element is different from the previous one
+        if (nums[i] !== nums[i - 1]) {
+            nums[index] = nums[i];  // Place the new unique element at the correct position
+            index++;  // Increment the index for the next unique element
+        }
+    }
+    return index;  // The number of unique elements
+}
+
