7310e2f7cb9f0962615161722acc0e9b08674d47
unknown <s88475@bht-berlin.de>
2024-12-23 22:54:08 +0100

    To do List created for Future Problems

---
+++ Future/problem_29.ts
@@ -0,0 +1,11 @@
+/**
+ * Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+ * @param dividend 
+ * @param divisor 
+ * @returns 
+ */
+
+function divide(dividend: number, divisor: number): number {
+
+    return 0
+};
