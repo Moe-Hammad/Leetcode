ef30918b11a4e4e9f11642c726df986dee48c1f4
Bemast3r <dahempvp@gmail.com>
2025-04-09 16:36:09 +0200

    Gelöst: Linked List

--- src/Problem_83.ts
+++ src/Problem_83.ts
@@ -8,7 +8,34 @@ class ListNode {
     }
 }
 
+function deleteDuplicatesGPT(head: ListNode | null): ListNode | null {
+    let current = head;
+
+    // Traverse the linked list
+    while (current && current.next) {
+        if (current.val === current.next.val) {
+            // Skip the duplicate node
+            current.next = current.next.next;
+        } else {
+            // Move to the next node
+            current = current.next;
+        }
+    }
+
+    return head;
+}
+
 
 function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let current = head;
 
+    while (current && current.next) {
+
+        if (current.val == current.next.val) {
+            current.next = current.next.next
+        } else {
+            current = current.next
+        }
+    }
+    return head
 };
